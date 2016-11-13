9ebfa68a8934b5f523361d0f577ce29a107e6755
Mackenzie Kary <mackenziekary@10.13.125.58>
2016-11-12 17:39:02 -0700

    new message v2

--- src/main/java/com/group7/distchat/Message.java
+++ src/main/java/com/group7/distchat/Message.java
@@ -25,6 +25,11 @@ public class Message
     public static final int M_OPEN = 1002;
     public static final int M_ECHO = 1003;
     public static final int M_ERROR = 1004;
+    public static final int M_QUIT = 1005;
+    public static final int M_MSG_ID = 1006;
+    public static final int M_USER_ID = 1007;
+    public static final int M_CHATROOM_ID = 1008;
+    public static final int M_MSG = 1009;
    
     //static patterns matching the messages
     public static Pattern msgSendPattern;
@@ -32,6 +37,11 @@ public class Message
     public static Pattern openPattern;
     public static Pattern echoPattern;
     public static Pattern errorPattern;
+    public static Pattern quitPattern;
+    public static Pattern msgIDPattern;
+    public static Pattern userIDPattern;
+    public static Pattern chatRoomIDPattern;
+    public static Pattern msgPattern;
 
     public int id = M_INVALID;
     //message type
@@ -45,7 +55,13 @@ public class Message
         registerPattern = Pattern.compile("^register\\r?\\n([\\w-]+)\\r?\\n",Pattern.DOTALL);
         openPattern = Pattern.compile("^open\\r?\\n([\\d]+)\\r?\\n",Pattern.DOTALL);
         echoPattern = Pattern.compile("^echo\\r?\\n(.+)\\r?\\n",Pattern.DOTALL);
-        errorPattern = Pattern.compile("^error\\r?\\n(.+)\\n",Pattern.DOTALL);
+        errorPattern = Pattern.compile("^error\\r?\\n(.+)\\r?\\n",Pattern.DOTALL);
+        quitPattern = Pattern.compile("^quit\\r?\\n\\r?\\n", Pattern.DOTALL);
+        msgIDPattern = Pattern.compile("^msgID\\r?\\n([0-9]+)\\r\\n", Pattern.DOTALL);
+        userIDPattern = Pattern.compile("^userID\\r?\\n([0-9]+)\\r\\n", Pattern.DOTALL);
+        chatRoomIDPattern = Pattern.compile("^chatRoomID\\r?\\n([0-9]+)\\r\\n", Pattern.DOTALL);
+        msgPattern = Pattern.compile("^msg\\r?\\n(.+)\\r\\n", Pattern.DOTALL);
+        
     }
 
     public Message ()
@@ -75,7 +91,12 @@ public class Message
                 || isRegister(messageString)
                 || isOpen(messageString)
                 || isEcho(messageString)
-                //|| isError(messageString)
+                || isError(messageString)
+                || isQuit(messageString)
+                || isMsgID(messageString)
+                || isUserID(messageString)
+                || isChatRoomID(messageString)
+                || isMsg(messageString)
                 );
         //use regex for now
     }
@@ -134,6 +155,36 @@ public class Message
                 m.find();
                 retMessageString = m.group(0);
             }
+            else if (isError(messageString)){
+                Matcher m = errorPattern.matcher(messageString);
+                m.find();
+                retMessageString = m.group(0);
+            }
+            else if (isQuit(messageString)){
+                Matcher m = quitPattern.matcher(messageString);
+                m.find();
+                retMessageString = m.group(0);
+            }
+            else if (isMsgID(messageString)){
+                Matcher m = msgIDPattern.matcher(messageString);
+                m.find();
+                retMessageString = m.group(0);
+            }
+            else if (isUserID(messageString)){
+                Matcher m = userIDPattern.matcher(messageString);
+                m.find();
+                retMessageString = m.group(0);
+            }
+            else if (isChatRoomID(messageString)){
+                Matcher m = chatRoomIDPattern.matcher(messageString);
+                m.find();
+                retMessageString = m.group(0);
+            }
+            else if (isMsg(messageString)){
+                Matcher m = msgPattern.matcher(messageString);
+                m.find();
+                retMessageString = m.group(0);
+            }
             retMessage = new Message(retMessageString);
             int byteLen = 0;
             try{
@@ -158,10 +209,16 @@ public class Message
         else if (isRegister(message))    return M_REGISTER;
         else if (isOpen(message))        return M_OPEN;
         else if (isEcho(message))        return M_ECHO;
+        else if (isError(message))       return M_ERROR;
+        else if (isQuit(message))      	 return M_QUIT;
+        else if (isMsgID(message))       return M_MSG_ID;
+        else if (isUserID(message))      return M_USER_ID;
+        else if (isChatRoomID(message))  return M_CHATROOM_ID;
+        else if (isMsg(message))  		 return M_MSG;
         else                             return M_INVALID; //not a valid message
     }
     /**
-     * Method to determine if the Mesage is of type messageSend
+     * Method to determine if the Message is of type messageSend
      * @param String message
      * @return boolean
      */
@@ -196,6 +253,36 @@ public class Message
     public static boolean isEcho(String message)
     {
         Matcher m = echoPattern.matcher(message);
+        return m.find();
+    }
+    public static boolean isError(String message)
+    {
+        Matcher m = errorPattern.matcher(message);
+        return m.find();
+    }
+    public static boolean isQuit(String message)
+    {
+        Matcher m = quitPattern.matcher(message);
+        return m.find();
+    }
+    public static boolean isMsgID(String message)
+    {
+        Matcher m = msgIDPattern.matcher(message);
+        return m.find();
+    }
+    public static boolean isUserID(String message)
+    {
+        Matcher m = userIDPattern.matcher(message);
+        return m.find();
+    }
+    public static boolean isChatRoomID(String message)
+    {
+        Matcher m = chatRoomIDPattern.matcher(message);
+        return m.find();
+    }
+    public static boolean isMsg(String message)
+    {
+        Matcher m = msgPattern.matcher(message);
         return m.find();
     }
     public int getMessageType ()
