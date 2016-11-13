87bcbdf82b44017ded937e99c7b75db009e3314a
dmarcus <dmarcus@ucalgary.ca>
2016-11-13 00:20:44 -0700

    Started Changes on Client

--- src/main/java/com/group7/distchat/Client.java
+++ src/main/java/com/group7/distchat/Client.java
@@ -4,66 +4,87 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.*;
+import java.net.UnknownHostException;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Client {
-	static String host = "";
+    public static Pattern ipWithPortPattern;
+    static 
+    {
+        ipWithPortPattern = Pattern.compile("(\\d?\\d?\\d\\.\\d?\\d?\\d\\.\\d?\\d?\\d\\.\\d?\\d?\\d):(\\d?\\d?\\d?\\d?\\d)"); //NOTE: ONLY IPV4 IS SUPPORTED
+    }
+
+    //Art is VERY important....
+    public static final String INIT_MESSAGE =
+ "\n\n\n\n"+
+ "/$$$$$$$ /$$$$$$ /$$$$$$ /$$$$$$$$/$$$$$$ /$$   /$$ /$$$$$$ /$$$$$$$$\n"+
+ "| $$__  $|_  $$_//$$__  $|__  $$__/$$__  $| $$  | $$/$$__  $|__  $$__/\n"+
+ "| $$  \\ $$ | $$ | $$  \\__/  | $$ | $$  \\__| $$  | $| $$  \\ $$  | $$\n"+   
+ "| $$  | $$ | $$ |  $$$$$$   | $$ | $$     | $$$$$$$| $$$$$$$$  | $$\n"+   
+ "| $$  | $$ | $$  \\____  $$  | $$ | $$     | $$__  $| $$__  $$  | $$\n"+   
+ "| $$  | $$ | $$  /$$  \\ $$  | $$ | $$    $| $$  | $| $$  | $$  | $$\n"+   
+ "| $$$$$$$//$$$$$|  $$$$$$/  | $$ |  $$$$$$| $$  | $| $$  | $$  | $$\n"+   
+ "|_______/|______/\\______/   |__/  \\______/|__/  |__|__/  |__/  |__/\n" +
+ "\nType \"help\" for a list of commands\nFor more info on the commands use \"help <command>\"\n\n";
+                                                                    
+	private static String host = "";
 	static int port = -1;
-	public static void main(String[] args) {
-		if (args.length != 2)
-			System.exit(1);
+	public static void main(String[] args) throws IOException //lazt fix
+    {
+	    Socket socket = null;
 
-		host	= args[0];
-		port	= Integer.parseInt( args[1] );
-		String toSend		= null;
-		String incoming = "";
-		
-		try {
-			Socket socket = new Socket(host, port);
-			if ( socket == null )		// catch NullPointerExceptions
-				return;
+        System.out.println(INIT_MESSAGE);
 
-			BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-			BufferedWriter sendToServer	= new BufferedWriter( new OutputStreamWriter( socket.getOutputStream() ) );
-			BufferedReader receiveFromServer = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
-
-			while(true){		
-				// Get keyboard input from user, to send to server
-				System.out.print("Input Message: ");
-				System.out.flush();
-				toSend = inFromUser.readLine();
-				
-				if (toSend.equals("quit"))		//Can change this later, just need a way to break out of loop
-					break;
-				
-				//sendToServer.write("echo\n" + toSend + "\n");
-				//sendToServer.write("echo\nhello\n");
-				socket.getOutputStream().write(("echo\n"+toSend+"\n").getBytes());
-                //sendToServer.write("echo\n" + toSend + "\n");
-				//sendToServer.flush();
-				
-				// Response back from server
-				incoming = receiveFromServer.readLine();
-				//System.out.println("Incoming message: " + incoming);	//This is where we check to see if the server
-																		// responds with a "message received"
-				if (incoming.equals("Error")){
-					
-				}else if (incoming.equals("Failure")){
-					
-				}else{
-					// If the message was good (no issues), then the response should be just a confirmation
-					System.out.println("Message was received by server");
-				}
-				} //While 
-			//socket.close();
-			} // try
-
-		catch (Exception e) {
-
-			System.err.println( "ERROR: Got exception: " + e.getMessage() );
-			e.printStackTrace();
-			}
+        //reader for user input stdin
+        BufferedReader console = new BufferedReader( new InputStreamReader(System.in));
+		System.out.println("Please connect to a Server (ipaddress:port)");
+        String ip = "";
+        ip = console.readLine();
+        Matcher matcher = ipWithPortPattern.matcher(ip);
+        while (!matcher.find())
+        {
+            System.out.println("please enter a valid host");
+            ip = console.readLine();
+            matcher = ipWithPortPattern.matcher(ip);
+        }
+        host = matcher.group(1);
+        port = Integer.parseInt(matcher.group(2));
+        System.out.println("Attempting Connect on " + host + " on port " + port + "...");
+        try 
+        {
+			socket = new Socket(host, port);
+        }
+        catch (IOException e) //Something wrong with IO
+        {
+            System.out.println("Failure");
+            e.printStackTrace(); //do nothing....
+        }
+        
+        System.out.println("Success!");
+        //While true
+        //get input from user
+        //validate input
+        //if valid message
+        //  queue for sending
+        //  wait for interrupts from workers
+        while(true)
+        {
+        }
 	} // main
-
+    public class NetworkReader extends Thread
+    {
+        public void run()
+        {
+        }
+    }
+    public class NetworkWriter extends Thread
+    {
+        public void run()
+        {
+        }
+    }
 }
